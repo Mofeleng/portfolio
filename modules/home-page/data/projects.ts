fdad26b957ad1bb46e95fac5a9e2ddc0df788e75
Mofeleng <mofelengmokgope@gmail.com>
2026-03-13 19:00:20 +0200

    Updated comind link

--- modules/home-page/data/projects.ts
+++ modules/home-page/data/projects.ts
@@ -4,7 +4,7 @@ export const projects: Project[] = [
         id: 1,
         title: "Comind | AI meeting platform",
         description: "An AI-powered collaboration tool that lets users create agents and hold real-time video meetings with them, think Zoom, but your participants are intelligent agents.",
-        link: "https://comind.zeta.vercel.app/",
+        link: "https://comind-zeta.vercel.app/",
         github: "https://github.com/Mofeleng/comind",
         skills: ["Typescript", "Next.js", "React", "AI SDK", "Open AI Realtime API", "Stream SDK", "Inngest", "Authentication"]
     }
